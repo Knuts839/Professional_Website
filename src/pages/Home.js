8670bac64df4f88ce0e550e775e6d70c1eabb483
Ravi <knuts839@umn.edu>
2021-01-13 12:52:53 -0600

    Home landing page nearly complete along with one project the password generator

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,20 +8,24 @@ const Home = () => {
 <div className="row">
     <div class="col-sm-6">
     <div class="card" width= '18rem'>
-            <img src="https://knuts839.github.io/Portfolio/PenradLogo.png" class="card-img-top" alt=" not rendering"/>
+            <img src="https://knuts839.github.io/Portfolio/WorkExperience.jpg" class="card-img-top" alt=" not rendering"/>
             <div class="card-body">
-                <h5 class="card-title">Work</h5>
-                <p class="card-text">I was a summer intern at Penrad Technologies inc. My responsibilities were to create the medical definitions used in the initial breast cancer ballot that was voted on by radiologists throughout the country.</p>
-                <p class="card-text">I did some light programing and code testing as well.</p>
-            </div>
+                <h5 class="card-title">Work Experiences</h5>
+                <p class="card-text">I have professional experience in consulting (ADVANTIS Research), IT (PenRad Technologies, ETI) and non-profit (DCRAC) settings.</p>
+                <p class="card-text">At ADVANTIS, I assisted with secondary research, data cleanup, setting up SPSS files, descriptive statistics, and editing.</p>
+                <p class="card-text">At PenRad Technologies, I worked on a biomedical quality assurance/document conversion project.  I also researched and developed definitions for a comprehensive list of breast cancer-related terms that formed the basis for a ballot of key opinion leaders in the field of radiology under the supervision of the CIO.</p>
+                <p class="card-text">At ETI, I assisted with the design, programming, and implementation of a Qualtrics research project. </p>
+                <p class="card-text">At DCRAC I produced short E-Learning modules using Articulate Storyline. </p>
+           </div>
         </div>
     </div>
     <div class="col-sm-6">
     <div class="card" width= '18rem'>
             <img src="https://knuts839.github.io/Portfolio/UofM.png" class="card-img-top" alt=" not rendering"/>
             <div class="card-body">
-                <h5 class="card-title">Education</h5>
-                <p class="card-text">I was a summer intern at Penrad Technologies inc. My responsibilities were to create the medical definitions used in the initial breast cancer ballot that was voted on by radiologists throughout the country.</p>
+                <h5 class="card-title">Education at the University of Minnesota</h5>
+                <p class="card-text">I completed the Full Stack Developer Coding Bootcamp at the University of Minnesota (December 2020).</p>
+                <p class="card-text">I also hold a Master’s degree in Human Resource Development from the University of Minnesota (2016). My Master’s coursework included a concentration in Human Factors/Design.</p>
             </div>
         </div>
     </div>
@@ -29,21 +33,19 @@ const Home = () => {
 <div class="row">
     <div class="col-sm-6">
     <div class="card" width= '18rem'>
-            <img src="https://knuts839.github.io/Portfolio/Penrad.jpg" class="card-img-top" alt=" not rendering"/>
+            <img src="https://knuts839.github.io/Portfolio/Gustavus.png" class="card-img-top" alt=" not rendering"/>
             <div class="card-body">
                 <h5 class="card-title">Education</h5>
-                <p class="card-text">I was a summer intern at Penrad Technologies inc. My responsibilities were to create the medical definitions used in the initial breast cancer ballot that was voted on by radiologists throughout the country.</p>
-                <p class="card-text">I did some light programing and code testing as well.</p>
+                <p class="card-text">a Bachelor’s degree in Sociology and Anthropology from Gustavus Adolphus College (2013). </p>
             </div>
         </div>
     </div>
     <div class="col-sm-6">
     <div class="card" width= '18rem'>
-            <img src="https://knuts839.github.io/Portfolio/Penrad.jpg" class="card-img-top" alt=" not rendering"/>
+            <img src="https://knuts839.github.io/Portfolio/Skills.jpg"class="card-img-top" alt=" not rendering"/>
             <div class="card-body">
                 <h5 class="card-title">Skills</h5>
-                <p class="card-text">I was a summer intern at Penrad Technologies inc. My responsibilities were to create the medical definitions used in the initial breast cancer ballot that was voted on by radiologists throughout the country.</p>
-                <p class="card-text">I did some light programing and code testing as well.</p>
+                <p class="card-text">My skills include the following technologies: HTML5, CSS5. JavaScript, JQuery, Bootstrap, Express.js, React.js, Node.Js, MongoDB, MySQL, Command Line, Progressive Web Apps, and Git.</p>
             </div>
         </div>
     </div>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -11,8 +11,7 @@ const Portfolio = () => {
             <img src="https://knuts839.github.io/Portfolio/PasswordGenerator.jpg" class="card-img-top" alt=" not rendering"/>
             <div class="card-body">
                 <h5 class="card-title">PasswordGenerator</h5>
-                <p class="card-text">I was a summer intern at Penrad Technologies inc. My responsibilities were to create the medical definitions used in the initial breast cancer ballot that was voted on by radiologists throughout the country.</p>
-                <p class="card-text">I did some light programing and code testing as well.</p>
+                <p class="card-text">Password generator this was my third assignment in the boot camp. This application required validation so that the user was forced to request at least and 8-character password and no more than a 128-character password. The password generator had to have user choice of four-character sets (numbers, Uppercase letters, Lowercase letters, and special characters).  In these yes or no prompts for the various cases I included validation that forces the user to write y for yes or an n for no and had a line of code that if the user wrote an uppercase y or n it would just be forced to lowercase and considered a valid response. For anything that required a user response I had code for validation to make sure it was correct if not I would just give them that same prompt again. </p>
             </div>
         </div>
     </div>
@@ -33,7 +32,6 @@ const Portfolio = () => {
             <div class="card-body">
                 <h5 class="card-title">Education</h5>
                 <p class="card-text">I was a summer intern at Penrad Technologies inc. My responsibilities were to create the medical definitions used in the initial breast cancer ballot that was voted on by radiologists throughout the country.</p>
-                <p class="card-text">I did some light programing and code testing as well.</p>
             </div>
         </div>
     </div>
